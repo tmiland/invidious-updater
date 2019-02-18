f0aa424b346d8736d6bb20f52c7a823f1e769ce3
Tommy Miland <kontakt@tmiland.com>
2019-02-18 15:31:11 +0100

    Update invidious_update.sh
    Added MIT license.

--- invidious_update.sh
+++ invidious_update.sh
@@ -4,7 +4,7 @@ CURRDIR=$(pwd)
 sfp=$(readlink -f "${BASH_SOURCE[0]}" 2>/dev/null || greadlink -f "${BASH_SOURCE[0]}" 2>/dev/null)
 if [ -z "$sfp" ]; then sfp=${BASH_SOURCE[0]}; fi
 SCRIPT_DIR=$(dirname "${sfp}")
-## Author: Tommy Miland (@tmiland) - 2019
+## Author: Tommy Miland (@tmiland) - Copyright (c) 2019
 ######################################################################
 ####                    Invidious Update.sh                       ####
 ####            Automatic update script for Invidio.us            ####
@@ -12,6 +12,33 @@ SCRIPT_DIR=$(dirname "${sfp}")
 ####                   Maintained by @tmiland                     ####
 ######################################################################
 version='1.2.3' # Must stay on line 14 for updater to fetch the numbers
+
+#------------------------------------------------------------------------------------#
+#                                                                                    #
+#   MIT License                                                                      #
+#                                                                                    #
+#   Copyright (c) 2019 Tommy Miland                                                  #
+#                                                                                    #
+#   Permission is hereby granted, free of charge, to any person obtaining a copy     #
+#   of this software and associated documentation files (the "Software"), to deal    #
+#   in the Software without restriction, including without limitation the rights     #
+#   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell        #
+#   copies of the Software, and to permit persons to whom the Software is            #
+#   furnished to do so, subject to the following conditions:                         #
+#                                                                                    #
+#   The above copyright notice and this permission notice shall be included in all   #
+#   copies or substantial portions of the Software.                                  #
+#                                                                                    #
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       #
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         #
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      #
+#   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           #
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    #
+#   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE    #
+#   SOFTWARE.                                                                        #
+#                                                                                    #
+#------------------------------------------------------------------------------------#
+
 # Colors used for printing
 RED='\033[0;31m'
 BLUE='\033[0;34m'
