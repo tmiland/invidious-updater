105a1a855b51c661c13452dc2af6a0efc9af4686
Tommy Miland <kontakt@tmiland.com>
2019-03-27 19:55:57 +0100

    Revert "Update invidious_update.sh"
    This reverts commit 84e50203023dceb0a3326a2648e23cdbaea24764.

--- invidious_update.sh
+++ invidious_update.sh
@@ -790,10 +790,10 @@ GetMaster () {
 ##
 UpdateMaster () {
   ignore_config
-  git stash
   currentVersion=$(git rev-list --max-count=1 --abbrev-commit HEAD)
   git pull
   for i in `git rev-list --abbrev-commit $currentVersion..HEAD` ; do file=${REPO_DIR}/config/migrate-scripts/migrate-db-$i.sh ; [ -f $file ] && $file ; done
+  git stash
   git checkout origin/${IN_BRANCH} -B ${IN_BRANCH}
 }
 ##
@@ -810,12 +810,12 @@ GetRelease () {
 ##
 UpdateRelease () {
   ignore_config
-  git stash
   currentVersion=$(git rev-list --max-count=1 --abbrev-commit HEAD)
   git pull
   latestVersion=$(git describe --tags --abbrev=0)
   #git checkout $latestVersion
   for i in `git rev-list --abbrev-commit $currentVersion..HEAD` ; do file=${REPO_DIR}/config/migrate-scripts/migrate-db-$i.sh ; [ -f $file ] && $file ; done
+  git stash
   git checkout tags/$latestVersion -B ${IN_RELEASE}
 }
 ##
