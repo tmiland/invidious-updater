1c12e80cc798d4c9375826bb0593f4a7f82f8a7f
Tommy Miland <kontakt@tmiland.com>
2019-11-15 17:35:32 +0100

    bump version

--- invidious_update.sh
+++ invidious_update.sh
@@ -11,7 +11,7 @@
 ####                   Maintained by @tmiland                     ####
 ######################################################################
 
-version='1.4.1' # Must stay on line 14 for updater to fetch the numbers
+version='1.4.2' # Must stay on line 14 for updater to fetch the numbers
 
 #------------------------------------------------------------------------------#
 #
