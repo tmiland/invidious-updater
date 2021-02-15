70673e0e3fb98bb0abca3bbeeac3158579c35f7e
Tommy Miland <kontakt@tmiland.com>
2021-02-15 21:22:10 +0100

    Update to v1.5.6
    Update script to v1.5.6
    
    - Added option to install cert from Let's Encrypt
    - Fixed Docker detection and install for Linux Mint
    - Minor refactor
    - ChellCheck fixes

--- invidious_update.sh
+++ invidious_update.sh
@@ -11,7 +11,7 @@
 ####                   Maintained by @tmiland                     ####
 ######################################################################
 
-version='1.5.5' # Must stay on line 14 for updater to fetch the numbers
+version='1.5.6' # Must stay on line 14 for updater to fetch the numbers
 
 #------------------------------------------------------------------------------#
 #
