57d03bed27555444ffc540e844e3ecdda65e90bd
Tommy Miland <kontakt@tmiland.com>
2022-11-08 19:35:44 +0100

    Disable shellchecks

--- invidious_update.sh
+++ invidious_update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+# shellcheck disable=SC2181,SC2059,SC2086,SC2002,SC2162,SC2005,SC2015,SC2308,SC2006,SC2236,SC2231,SC1091
 
 ## Author: Tommy Miland (@tmiland) - Copyright (c) 2022
 
@@ -115,7 +115,7 @@ CAPTCHA_KEY=${CAPTCHA_KEY:-}
 # Default Swap option
 SWAP_OPTIONS=${SWAP_OPTIONS:-n}
 # Docker compose repo name
-COMPOSE_REPO_NAME="docker/compose"
+# COMPOSE_REPO_NAME="docker/compose"
 # Docker compose version
 DOCKER_COMPOSE_VER=1.25.0
 # Logfile
