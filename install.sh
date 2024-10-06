2eb9d13821b53ff7f9b5bdbe9b3fc1be3eac2c92
Tommy Miland <kontakt@tmiland.com>
2024-10-06 17:01:55 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+if [[ $1 == "release" ]]
+then
+  mkdir -p /opt/invidious_update && \
+    curl -sSL https://github.com/tmiland/Invidious-Updater/releases/latest/download/invidious_update.sh > /opt/invidious_update/invidious_update.sh && \
+    chmod +x /opt/invidious_update/invidious_update.sh && \
+    ln -s /opt/invidious_update/invidious_update.sh /usr/local/bin/invidious_update && \
+    invidious_update
+else
+  mkdir -p /opt/invidious_update && \
+    curl -sSL https://github.com/tmiland/Invidious-Updater/raw/master/invidious_update.sh > /opt/invidious_update/invidious_update.sh && \
+    chmod +x /opt/invidious_update/invidious_update.sh && \
+    ln -s /opt/invidious_update/invidious_update.sh /usr/local/bin/invidious_update && \
+    invidious_update
+fi
