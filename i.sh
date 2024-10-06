f748dce42eec6462502e2d38f39d1448e12b3994
Tommy Miland <kontakt@tmiland.com>
2024-10-06 17:41:12 +0200

    Update i.sh

--- i.sh
+++ i.sh
@@ -2,10 +2,10 @@
 if [[ $1 == "release" ]]
 then
   curl -sSL https://github.com/tmiland/Invidious-Updater/releases/latest/download/invidious_update.sh > invidious_update.sh && \
-  chmod +x invidious_update.sh && \
-  /invidious_update.sh -i
+  chmod +x invidious_update.sh
+  . /invidious_update.sh -i
 else
   curl -sSL https://github.com/tmiland/Invidious-Updater/raw/master/invidious_update.sh > invidious_update.sh && \
-  chmod +x invidious_update.sh && \
-  ./invidious_update.sh -i
+  chmod +x invidious_update.sh
+  . ./invidious_update.sh -i
 fi
