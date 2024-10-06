182c5b6b7a910bdba906f4bf90135e15e3fd57ab
Tommy Miland <kontakt@tmiland.com>
2024-10-06 18:36:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ then
     curl -sSL https://github.com/tmiland/Invidious-Updater/releases/latest/download/invidious_update.sh > /opt/invidious_update/invidious_update.sh && \
     chmod +x /opt/invidious_update/invidious_update.sh && \
     ln -s /opt/invidious_update/invidious_update.sh /usr/local/bin/invidious_update
-    invidious_update
 else
   mkdir -p /opt/invidious_update && \
     curl -sSL https://github.com/tmiland/Invidious-Updater/raw/master/invidious_update.sh > /opt/invidious_update/invidious_update.sh && \
     chmod +x /opt/invidious_update/invidious_update.sh && \
     ln -s /opt/invidious_update/invidious_update.sh /usr/local/bin/invidious_update
-    invidious_update
 fi
+
+invidious_update "$@"
