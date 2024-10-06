8db09d6a00c5a8647668cd2fcc816d4f51b9ef17
Tommy Miland <kontakt@tmiland.com>
2024-10-06 18:00:44 +0200

    Update i.sh

--- i.sh
+++ i.sh
@@ -9,11 +9,11 @@ if [ -z "$sfp" ]; then sfp=${BASH_SOURCE[0]}; fi
 SCRIPT_DIR=$(dirname "${sfp}")
 if [[ $1 == "release" ]]
 then
-  curl -sSL https://github.com/tmiland/Invidious-Updater/releases/latest/download/invidious_update.sh > invidious_update.sh && \
-  chmod +x invidious_update.sh
-  "${SCRIPT_DIR}/${SCRIPT_FILENAME}" -i
+  curl -sSL https://github.com/tmiland/Invidious-Updater/releases/latest/download/invidious_update.sh > "${SCRIPT_DIR}/${SCRIPT_FILENAME}" && \
+  chmod +x "${SCRIPT_DIR}/${SCRIPT_FILENAME}"
+  . "${SCRIPT_DIR}/${SCRIPT_FILENAME}" -i
 else
-  curl -sSL https://github.com/tmiland/Invidious-Updater/raw/master/invidious_update.sh > invidious_update.sh && \
-  chmod +x invidious_update.sh
-  "${SCRIPT_DIR}/${SCRIPT_FILENAME}" -i
+  curl -sSL https://github.com/tmiland/Invidious-Updater/raw/master/invidious_update.sh > "${SCRIPT_DIR}/${SCRIPT_FILENAME}" && \
+  chmod +x "${SCRIPT_DIR}/${SCRIPT_FILENAME}"
+  . "${SCRIPT_DIR}/${SCRIPT_FILENAME}" -i
 fi
